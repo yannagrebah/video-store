ce2a1893407d9658980c0d8070283811064a5c3b
yannagrebah <195680151+yannagrebah@users.noreply.github.com>
2026-03-11 11:58:03 +0000

    update next.config.js Allow TMDB remote images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [new URL("https://image.tmdb.org/**")],
+  },
+};
 
 export default config;
