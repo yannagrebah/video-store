db3bc7cae04449be309320e6300090ed6e5b6fbf
yannagrebah <195680151+yannagrebah@users.noreply.github.com>
2026-03-12 14:08:18 +0000

    update homepage add movie-ai-agent to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
 import { HydrateClient } from "~/trpc/server";
 import Cart from "./_components/cart";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import MovieAIAgent from "~/components/movie-ai-agent";
+import { Bot } from "lucide-react";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main className="bg-background min-h-screen">
-        <nav className="border-b-2 p-2">
+        <nav className="border-b-2 p-2 md:grid md:grid-flow-col md:items-center md:justify-between">
           <Link href="/">
             <h1 className="font-display text-center text-2xl font-bold tracking-tight uppercase md:text-left">
               VideoStore
             </h1>
           </Link>
+          <MovieAIAgent>
+            <Button
+              variant="outline"
+              size={"icon"}
+              className="absolute right-4 bottom-4 md:relative md:right-0 md:bottom-0"
+            >
+              <Bot className="size-4" />
+            </Button>
+          </MovieAIAgent>
         </nav>
         <section className="space-y-4 px-4 py-6">
           <Cart />
