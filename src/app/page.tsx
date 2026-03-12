1ef2ead20855134db5591a6f8e98b5675d90a135
yannagrebah <195680151+yannagrebah@users.noreply.github.com>
2026-03-12 09:59:47 +0000

    Update homepage styling and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,14 @@ export default async function Home() {
       <main className="bg-background min-h-screen">
         <nav className="border-b-2 p-2">
           <Link href="/">
-            <h1 className="font-display text-2xl font-bold tracking-tight">
+            <h1 className="font-display text-center text-2xl font-bold tracking-tight uppercase md:text-left">
               VideoStore
             </h1>
           </Link>
         </nav>
-        <Cart />
+        <section className="space-y-4 px-4 py-6">
+          <Cart />
+        </section>
       </main>
     </HydrateClient>
   );
