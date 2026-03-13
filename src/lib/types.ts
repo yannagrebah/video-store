ade12a1367d51c472021dc72eeabc4aabcf253e9
yannagrebah <195680151+yannagrebah@users.noreply.github.com>
2026-03-13 11:56:29 +0000

    Update types.ts Add MoviePerson and MovieDetails schemas

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,16 +1,66 @@
 import z from "zod";
 
-export const movieSchema = z.object({
+const movieSchema = z.object({
   id: z.number(),
   title: z.string(),
   release_date: z.string().optional(),
   poster_path: z.string().nullable(),
 });
 
+const moviePersonSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  gender: z.number().transform((val) => {
+    switch (val) {
+      case 0:
+        return "Not set / not specified";
+      case 1:
+        return "Female";
+      case 2:
+        return "Male";
+      case 3:
+        return "Non-binary";
+      default:
+        return "Unknown";
+    }
+  }),
+  know_for_department: z.string(),
+  known_for: z.array(
+    z.object({
+      id: z.number(),
+      title: z.string().optional(),
+      media_type: z.string(),
+      release_date: z.string().optional(),
+    }),
+  ),
+});
+
+const movieDetaislSchema = movieSchema.extend({
+  overview: z.string().optional(),
+  genres: z.array(z.object({ id: z.number(), name: z.string() })).optional(),
+  credits: z.object({
+    cast: z.array(
+      moviePersonSchema.omit({ known_for: true }).extend({
+        character: z.string().optional(),
+      }),
+    ),
+    crew: z.array(
+      moviePersonSchema.omit({ known_for: true }).extend({
+        job: z.string().optional(),
+      }),
+    ),
+  }),
+});
+
 type Movie = z.infer<typeof movieSchema>;
+
+type MoviePerson = z.infer<typeof moviePersonSchema>;
+
+type MovieDetails = z.infer<typeof movieDetaislSchema>;
 
 interface MovieCart extends Movie {
   quantity: number;
 }
 
-export type { Movie, MovieCart };
+export type { Movie, MovieCart, MoviePerson, MovieDetails };
+export { movieSchema, moviePersonSchema, movieDetaislSchema };
