68c062fc7c2a5de953ab59b4310fa2609a157952
yannagrebah <195680151+yannagrebah@users.noreply.github.com>
2026-03-15 23:43:37 +0000

    Update homepage layout Add link button to admin dashboard

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,10 +1,12 @@
-import "~/styles/globals.css";
-
+import { Bot } from "lucide-react";
 import { type Metadata } from "next";
 import { Audiowide, Roboto } from "next/font/google";
-
-import { TRPCReactProvider } from "~/trpc/react";
+import Link from "next/link";
+import MovieAIAgent from "~/components/movie-ai-agent";
+import { Button } from "~/components/ui/button";
 import { TooltipProvider } from "~/components/ui/tooltip";
+import "~/styles/globals.css";
+import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
   title: "Video Store",
@@ -30,7 +32,32 @@ export default function RootLayout({
     <html lang="en" className={`dark ${roboto.variable} ${audiowide.variable}`}>
       <body>
         <TooltipProvider>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
+          <TRPCReactProvider>
+            <main className="bg-background min-h-screen">
+              <nav className="inline-flex w-full items-center justify-between border-b-2 p-2">
+                <Link href="/">
+                  <h1 className="font-display text-center text-2xl font-bold tracking-tight uppercase md:text-left">
+                    VideoStore
+                  </h1>
+                </Link>
+                <span className="inline-flex items-center gap-2">
+                  <Button variant="ghost" className="hover:bg-muted/50" asChild>
+                    <Link href="/admin">Admin Dashboard</Link>
+                  </Button>
+                  <MovieAIAgent>
+                    <Button
+                      variant="outline"
+                      size={"icon"}
+                      className="absolute right-4 bottom-4 md:relative md:right-0 md:bottom-0"
+                    >
+                      <Bot className="size-4" />
+                    </Button>
+                  </MovieAIAgent>
+                </span>
+              </nav>
+              {children}
+            </main>
+          </TRPCReactProvider>
         </TooltipProvider>
       </body>
     </html>
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,27 +14,9 @@ export default async function Home({
   const invoiceId = (await searchParams).purchaseSuccess;
   return (
     <HydrateClient>
-      <main className="bg-background min-h-screen">
-        <nav className="border-b-2 p-2 md:grid md:grid-flow-col md:items-center md:justify-between">
-          <Link href="/">
-            <h1 className="font-display text-center text-2xl font-bold tracking-tight uppercase md:text-left">
-              VideoStore
-            </h1>
-          </Link>
-          <MovieAIAgent>
-            <Button
-              variant="outline"
-              size={"icon"}
-              className="absolute right-4 bottom-4 md:relative md:right-0 md:bottom-0"
-            >
-              <Bot className="size-4" />
-            </Button>
-          </MovieAIAgent>
-        </nav>
-        <section className="space-y-4 px-4 py-6">
-          {invoiceId ? <PurchaseSuccess invoiceId={invoiceId} /> : <Cart />}
-        </section>
-      </main>
+      <section className="space-y-4 px-4 py-6">
+        {invoiceId ? <PurchaseSuccess invoiceId={invoiceId} /> : <Cart />}
+      </section>
     </HydrateClient>
   );
 }
